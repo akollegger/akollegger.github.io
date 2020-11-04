6f9b8ba695024f04e37d20723014de0fd2a2e0a2
Andreas Kollegger <akollegger@gmail.com>
2020-11-04 17:12:43 +0100

    added dev.to badge

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,3 +5,9 @@
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+
+<div>
+	<a href="https://dev.to/akollegger">
+		<img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Andreas Kollegger's DEV Profile" height="30" width="30">
+	</a>
+</div>
